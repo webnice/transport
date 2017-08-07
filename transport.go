8ba9f700d5a606879c6c98b089fe1bde58a77a00
Alex Geer <monoflash@gmail.com>
2017-08-07 10:09:50 +0300

    Remove warning

--- transport.go
+++ transport.go
@@ -3,7 +3,6 @@ package transport
 //import "gopkg.in/webnice/debug.v1"
 //import "gopkg.in/webnice/log.v2"
 import (
-	"log"
 	"os"
 	"runtime"
 
@@ -22,13 +21,9 @@ func NewMethod() methods.Interface { return methods.New() }
 
 // destructor Вызывается при уничтожении объекта
 func destructor(obj *implementation) {
-	//log.Debug(" --- Запуск деструктора")
 	for i := range obj.CollectionOfTemporaryFiles {
-		//log.Debug(" ---- Удаление файла: %s", obj.CollectionOfTemporaryFiles[i])
 		if obj.CollectionOfTemporaryFiles[i] != "" {
-			if err := os.Remove(obj.CollectionOfTemporaryFiles[i]); err != nil {
-				log.Printf("Warning, delete temporary file '%s' error: %s", obj.CollectionOfTemporaryFiles[i], err)
-			}
+			_ = os.Remove(obj.CollectionOfTemporaryFiles[i])
 		}
 	}
 }
@@ -45,12 +40,12 @@ func (t *implementation) TemporaryFile(fileName string) {
 // NewRequest Создание нового запроса
 func (t *implementation) NewRequest(m methods.Value) Request {
 	var req = new(requestImplementation)
+
 	if m != nil {
 		req.RequestMethod = m
 	} else {
 		req.RequestMethod = methods.New().Get()
 	}
-
 	// Установка функции коллекционирования временных файлов
 	req.collectionOfTemporaryFilesFn = t.TemporaryFile
 
